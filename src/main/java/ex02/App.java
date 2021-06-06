457215b523c716af7e0dd0f678265492a8ba0df4
Zachary Flanagan <zacharyf24147@gmail.com>
2021-06-06 06:26:07 -0400

    Second Commit

--- src/main/java/ex02/App.java
+++ src/main/java/ex02/App.java
@@ -1,3 +1,21 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Zachary Flanagan
+ */
+
 package ex02;
+
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String inputString = in.nextLine();
+
+        int charNum = inputString.length( );
+
+        String outputString = inputString + " has " + charNum + " characters.";
+        System.out.println(outputString);
+    }
 }
